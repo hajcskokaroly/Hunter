0d5b931bbb93c52215f65dbbef4eec73e8ca9078
hajcs <hajcs@K4R3ESZ>
2022-11-23 14:27:01 +0100

    Add project files.

---
+++ Hunter/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunter
+{
+    abstract class Animal
+    {
+        const double basePrice = 100000;
+
+        public new virtual string GetType() { return " pémének ára: "; }
+
+        public virtual double getPrice() { return basePrice; }
+    }
+}
---
+++ Hunter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Animal[] animals = new Animal[3];
+
+
+            animals[0] = new Squirrel();
+            animals[1]=new Rabbit();
+            animals[2]=new PolarBear();
+
+            for (int i = 0; i < animals.Length; i++)
+                Console.WriteLine(animals[i].GetType()+ animals[i].getPrice());
+            Console.ReadKey();
+        }
+    }
+}
